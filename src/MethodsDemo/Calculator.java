e6a0d68d5a594bdb798fe2bc93d65dcc1413ed3b
VisionAutomationTesting <visionautomationtesting@gmail.com>
2025-02-12 08:37:46 +0530

    method demo changes added

---
+++ src/MethodsDemo/Calculator.java
@@ -0,0 +1,17 @@
+package MethodsDemo;
+
+public class Calculator {
+
+	// Methods
+	
+	void add() {
+		System.out.println("Add method");
+	}
+	
+	public static void main(String[] args) {
+
+
+
+	}
+
+}
